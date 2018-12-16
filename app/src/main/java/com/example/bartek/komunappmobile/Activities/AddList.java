bb532ab0d4213bce34f0c17c6332b41227f41c98
Bartłomiej Sikora <bartlomiej.sikora@student.wat.edu.pl>
2018-12-17 00:02:37 +0100

    -better frontend on creating shoppingList

--- app/src/main/java/com/example/bartek/komunappmobile/Activities/AddList.java
+++ app/src/main/java/com/example/bartek/komunappmobile/Activities/AddList.java
@@ -4,24 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.bartek.komunappmobile.R;
 
 public class AddList extends AppCompatActivity {
 
-    EditText editText ;
+    TextView nameText ;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_list);
-        editText = findViewById(R.id.addListName);
+        nameText = findViewById(R.id.addListName);
         Intent i = getIntent();
         String listName;
         listName = i.getStringExtra("ListName");
         Log.e("name", listName);
-        editText.setText(listName);
+        nameText.setText(listName);
+    }
+
+    public void accept(View view) {
+        finish();
     }
 }
