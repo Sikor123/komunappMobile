39d4c89e14743a9bd6b54b84aeb514b97c04fa84
Bartłomiej Sikora <bartlomiej.sikora@student.wat.edu.pl>
2018-12-18 00:08:13 +0100

    -better Logging activity(headers) -login->shopping

--- app/src/main/java/com/example/bartek/komunappmobile/Activities/LoginActivity.java
+++ app/src/main/java/com/example/bartek/komunappmobile/Activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.bartek.komunappmobile.Activities;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,23 +9,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Header;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.bartek.komunappmobile.R;
+import com.example.bartek.komunappmobile.jsonObjectRequest.LoginRequest;
 import com.example.bartek.komunappmobile.jsony.LoginBody;
 import com.google.gson.Gson;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class LoginActivity extends AppCompatActivity {
     String URL = "http://192.168.0.2";
     String port = "8080";
     String devApi = "/user/sign-up";
     Gson gson = new Gson();
+    Header[] headers ;
     public void tapLogin(View v) {
         EditText login = findViewById(R.id.editLogin);
         EditText password = findViewById(R.id.editPassword);
@@ -32,7 +42,7 @@ public class LoginActivity extends AppCompatActivity {
         LoginBody loginBody = new LoginBody(login.getText().toString() ,  password.getText().toString());
         Log.e("RestResponse" , gson.toJson(loginBody));
         RequestQueue requestQueue = Volley.newRequestQueue(this);
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
+        LoginRequest objectRequest = new LoginRequest(
                 Request.Method.GET,
                 "https://jsonplaceholder.typicode.com/todos/1",
                 null,
@@ -52,6 +62,8 @@ public class LoginActivity extends AppCompatActivity {
         );
 
         requestQueue.add(objectRequest);
+        Intent intent = new Intent(this,ShoppingActivity.class);
+        startActivity(intent);
 
     }
 
